b5dd9f468b025012eda83bece1f85c1617d40b21
Vincent Dlugosch <vincent.dlugosch@zalf.de>
2025-03-24 16:58:03 +0100

    misc: change some small stuff

---
+++ Mas.Infrastructure.ServiceRegistry.Test/RegistryAdminTests.cs
@@ -0,0 +1,10 @@
+namespace Mas.Infrastructure.ServiceRegistry.Test;
+
+[TestClass]
+public class RegistryAdminTests
+{
+    [AssemblyInitialize]
+    public static void AdminTest(TestContext testContext)
+    {
+    }
+}
--- Mas.Infrastructure.ServiceRegistry/RegData.cs
+++ Mas.Infrastructure.ServiceRegistry/RegData.cs
@@ -5,12 +5,10 @@ namespace Mas.Infrastructure.ServiceRegistry;
 
 public struct RegData
 {
-    public string Id { get; set; }
 
     public R.Registry.Entry Entry { get; set; }
 
     public R.Registrar.IUnregisterCapability UnregisterCapability { get; set; }
-
 
     public R.Registrar.IReregisterCapability ReregisterCapability { get; set; }
 
